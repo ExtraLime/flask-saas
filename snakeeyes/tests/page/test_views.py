47e122ba945cade4ac206e8395dacb9e7da8275e
ExtraLime <willdox7@live.com>
2022-07-09 18:16:06 -0700

    touchups for .gitignore

--- snakeeyes/tests/page/test_views.py
+++ snakeeyes/tests/page/test_views.py
@@ -1,6 +1,7 @@
 from flask import url_for
 import re
 
+
 class TestPage(object):
     def test_home_page(self, client):
         """ Home page should respond with a success 200. """
@@ -20,10 +21,10 @@ class TestPage(object):
     def test_faq_page(self, client):
         """ faq page should respond with a success 200. """
         response = client.get(url_for('page.faq'))
-        assert response.status_code == 200 
-    
+        assert response.status_code == 200
+
     def test_valid_title(self, client):
         response = client.get(url_for('page.faq'))
-        title_pattern = re.search(r'<title>(.*?)</title>',str(response.data))
+        title_pattern = re.search(r'<title>(.*?)</title>', str(response.data))
         assert '<title>' in str(response.data)
         assert title_pattern
